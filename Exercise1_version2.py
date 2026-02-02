6ba736eff06b34a83e68d4d47c63b24682f7e39c
jpfnice <jpfnice@gmail.com>
2026-02-02 12:03:20 +0100

    Add files via upload

---
+++ Exercise1_version2.py
@@ -0,0 +1,24 @@
+"""
+Write a Python script that prompts the user for a number(an integer) and displays a message that indicates if the number is odd, even or zero.
+You can use the modulo operator (%) to determine if a number is even or not: 
+    value % 2 returns the remainder of the division of value by 2
+    If this remainder is 0 value is even
+    
+After having tested a first number, the script should prompt the user for other numbers, and continue to run as long as the user does not enter the string "stop".
+"""
+
+while True:
+    
+    nb=input("Please enter a numeric value or 'stop': ")
+    
+    if nb == 'stop':
+        break
+    
+    nb=int(nb)  # to convert the provided string into an int
+    
+    if nb == 0:
+        print(nb, "is Zero")
+    elif nb % 2 == 0:
+        print(nb, "is even")
+    else:
+        print(nb, "is odd")
---
+++ cosFunction.py
@@ -0,0 +1,5 @@
+import math
+result=math.cos(5.6) # cmath
+print(result)
+result=math.sin(5.6)
+print(result)
