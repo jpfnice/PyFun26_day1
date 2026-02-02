c7ea46c27f667aa93b1d8b2590337c365831798a
jpfnice <jpfnice@gmail.com>
2026-02-02 11:04:49 +0100

    Add files via upload

---
+++ Exercise1.py
@@ -0,0 +1,8 @@
+"""
+Write a Python script that prompts the user for a number(an integer) and displays a message that indicates if the number is odd, even or zero.
+You can use the modulo operator (%) to determine if a number is even or not: 
+    value % 2 returns the remainder of the division of value by 2
+    If this remainder is 0 value is even
+    
+After having tested a first number, the script should prompt the user for other numbers, and continue to run as long as the user does not enter the string "stop".
+"""
