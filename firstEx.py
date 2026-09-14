daa279e30f4b632c70ac70ce545606e0c509c2ae
jpfnice <jpfnice@gmail.com>
2026-09-14 11:30:43 +0200

    Add files via upload

---
+++ firstEx.py
@@ -0,0 +1,13 @@
+
+op1=123 # op1 is a variable initialized with the value 123. op1 is of type int
+print ('value of op1 is', op1)
+print()
+op2 = op1 * 5 # + - / * // ** %
+print(op2)
+
+print('10/3 is', 10/3)
+print('10//3 is', 10//3)
+
+print('2**3 is', 2**3) # 2 raised to the power 3
+print('10%3 is', 10%3) # remainder of the division of 10 by 3
+
---
+++ fourthEx.py
@@ -0,0 +1,10 @@
+
+op1=12  # OK
+print(op1)  # OK
+
+print("Second part of the script") # OK
+
+op2 = 12 + op3 # KO: Runtime error: attempt to use a non existing variable
+
+print(op2) # Not executed 
+
